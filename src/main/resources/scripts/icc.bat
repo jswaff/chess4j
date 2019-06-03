f72fecb38feede830a756f84f0470035e415b5f0
James Swafford <j.f.swafford@gmail.com>
2019-06-02 20:37:13 -0400

    converting to unix style EOL

--- src/main/resources/scripts/icc.bat
+++
@@ -1,7 +0,0 @@
-rem THIS IS A SAMPLE - ADJUST TO FIT YOUR NEEDS!
-
-winboard.exe -debug /fcp "java -jar chess4j-1.2-uber.jar" ^
-   /fd="c:\\Users\\James\\chess4j" ^
-   /ics /zp /icshost=207.99.83.228 /icshelper=timestamp ^
-   /autoflag /size=average 
-
--- src/main/resources/scripts/winboard.bat
+++
@@ -1,2 +0,0 @@
-winboard.exe /fcp "java -ea -jar chess4j-1.2-uber.jar" ^
-   /fd="c:\\chess4j" /autoflag /size=average /debug
