5615b3fc4d0ebfd87511a3a1f5efb42c6afa77d3
James Swafford <james.swafford@rainbowfinancellc.com>
2020-01-08 16:42:38 -0500

    added Evaluator interface for mocking

--- chess4j-java/src/main/java/com/jamesswafford/chess4j/search/AlphaBetaSearch.java
+++ chess4j-java/src/main/java/com/jamesswafford/chess4j/search/AlphaBetaSearch.java
@@ -3,7 +3,7 @@ package com.jamesswafford.chess4j.search;
 import com.jamesswafford.chess4j.board.Board;
 import com.jamesswafford.chess4j.board.Move;
 import com.jamesswafford.chess4j.board.Undo;
-import com.jamesswafford.chess4j.eval.Eval;
+import com.jamesswafford.chess4j.eval.Evaluator;
 import com.jamesswafford.chess4j.movegen.MoveGen;
 
 import java.util.List;
@@ -12,10 +12,12 @@ public class AlphaBetaSearch {
 
     private final Board board;
     private final SearchParameters searchParameters;
+    private final Evaluator evaluator;
 
-    public AlphaBetaSearch(Board board, SearchParameters searchParameters) {
+    public AlphaBetaSearch(Board board, SearchParameters searchParameters, Evaluator evaluator) {
         this.board = board;
         this.searchParameters = searchParameters;
+        this.evaluator = evaluator;
     }
 
     public int search() {
@@ -25,7 +27,7 @@ public class AlphaBetaSearch {
     private int search(int depth, int alpha, int beta) {
 
         if (depth == 0) {
-            return Eval.eval(board);
+            return evaluator.evaluateBoard(board);
         }
 
         List<Move> moves = MoveGen.genLegalMoves(board);
--- chess4j-java/src/test/java/com/jamesswafford/chess4j/search/AlphaBetaSearchTest.java
+++ chess4j-java/src/test/java/com/jamesswafford/chess4j/search/AlphaBetaSearchTest.java
@@ -1,22 +1,47 @@
 package com.jamesswafford.chess4j.search;
 
 import com.jamesswafford.chess4j.board.Board;
+import com.jamesswafford.chess4j.board.Move;
+import com.jamesswafford.chess4j.eval.Evaluator;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 import static com.jamesswafford.chess4j.Constants.INFINITY;
+import static com.jamesswafford.chess4j.pieces.Pawn.*;
+import static com.jamesswafford.chess4j.pieces.Knight.*;
+import static com.jamesswafford.chess4j.board.squares.Square.*;
 
 public class AlphaBetaSearchTest {
 
     @Test
-    public void testSearch_initialPos() {
+    public void testSearch_initialPos_depth1() {
 
+        // given a board in the initial position
         Board board = new Board();
+        Evaluator evaluator = mock(Evaluator.class);
+        SearchParameters params = new SearchParameters(1, -INFINITY, INFINITY);
 
-        SearchParameters params = new SearchParameters(3, -INFINITY, INFINITY);
-        AlphaBetaSearch alphaBetaSearch = new AlphaBetaSearch(board, params);
+        // set up the return scores for a couple of moves.  all others default to score=0.
+        // the returned scores are from black's point-of-view so the lower the better for white.
+        Board board1 = new Board();
+        board1.applyMove(new Move(WHITE_PAWN, E2, E4));
+        when(evaluator.evaluateBoard(board1)).thenReturn(3);
+
+        Board board2 = new Board();
+        board2.applyMove(new Move(WHITE_KNIGHT, B1, C3));
+        when(evaluator.evaluateBoard(board2)).thenReturn(-5);
+
+        // when the search is invoked
+        AlphaBetaSearch alphaBetaSearch = new AlphaBetaSearch(board, params, evaluator);
         int score = alphaBetaSearch.search();
 
-        System.out.println("search score: " + score);
+        // then the evaluator should have been invoked for each move
+        verify(evaluator, times(20)).evaluateBoard(any(Board.class));
+
+        // and the score should be the highest returned score
+        assertEquals(5, score);
     }
 
 }
