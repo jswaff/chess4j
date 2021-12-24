91f047318878936676bc8d9b0f6d21a9c7c480d5
James Swafford <j.f.swafford@gmail.com>
2021-12-24 08:55:36 -0500

    tuner writes progress to properties file

--- chess4j-java/src/main/java/com/jamesswafford/chess4j/tuner/LogisticRegressionTuner.java
+++ chess4j-java/src/main/java/com/jamesswafford/chess4j/tuner/LogisticRegressionTuner.java
@@ -3,10 +3,15 @@ package com.jamesswafford.chess4j.tuner;
 import com.jamesswafford.chess4j.Globals;
 import com.jamesswafford.chess4j.board.Board;
 import com.jamesswafford.chess4j.eval.EvalTermsVector;
+import com.jamesswafford.chess4j.io.EvalTermsVectorUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.Properties;
 
 public class LogisticRegressionTuner {
 
@@ -31,8 +36,8 @@ public class LogisticRegressionTuner {
 
     public EvalTermsVector optimize(EvalTermsVector evalTermsVector) {
         int numParams = evalTermsVector.terms.length;
-        double bestE = calculateAverageError(evalTermsVector);
-        LOGGER.info("initial E: " + bestE);
+        double bestError = calculateAverageError(evalTermsVector);
+        LOGGER.info("initial E: " + bestError);
         EvalTermsVector bestVector = new EvalTermsVector(evalTermsVector);
         int numIterations = 0;
         int numParamsImproved;
@@ -43,26 +48,26 @@ public class LogisticRegressionTuner {
             for (int i=0;i<numParams;i++) {
                 EvalTermsVector searchVector = new EvalTermsVector(bestVector);
                 searchVector.terms[i] = searchVector.terms[i] + 1;
-                double searchE = calculateAverageError(searchVector);
-                if (searchE < bestE) {
-                    bestE = searchE;
+                double error = calculateAverageError(searchVector);
+                if (error < bestError) {
+                    bestError = error;
                     bestVector = searchVector;
                     ++numParamsImproved;
                 } else {
                     searchVector.terms[i] = searchVector.terms[i] - 2;
-                    searchE = calculateAverageError(searchVector);
-                    if (searchE < bestE) {
-                        bestE = searchE;
+                    error = calculateAverageError(searchVector);
+                    if (error < bestError) {
+                        bestError = error;
                         bestVector = searchVector;
                         ++numParamsImproved;
                     }
                 }
             }
-            LOGGER.info("iteration " + numIterations + ": bestE=" + bestE + ", numParamsImproved=" + numParamsImproved);
+            LOGGER.info("iteration " + numIterations + ": E=" + bestError + ", numParamsImproved=" + numParamsImproved);
 
+            // write to file in case execution is interrupted
+            writeVectorToTempProperties(bestVector);
         } while (numParamsImproved > 0);
-
-
         return bestVector;
     }
 
@@ -82,4 +87,14 @@ public class LogisticRegressionTuner {
         return totalError / gameRecords.size();
     }
 
+    private void writeVectorToTempProperties(EvalTermsVector evalTermsVector) {
+        Properties props = EvalTermsVectorUtil.toProperties(evalTermsVector);
+        LOGGER.info(props);
+        try (OutputStream output = new FileOutputStream("eval-tuning.properties")) {
+            props.store(output, null);
+        } catch (IOException e) {
+            LOGGER.error(e);
+            throw new RuntimeException(e);
+        }
+    }
 }
