bbc0a8d09174d6bb13b61ed3319d75fbddc6d83d
James Swafford <j.f.swafford@gmail.com>
2022-04-21 22:04:43 -0400

    disable decay rate

--- chess4j-java/src/main/java/com/jamesswafford/chess4j/tuner/LogisticRegressionTuner.java
+++ chess4j-java/src/main/java/com/jamesswafford/chess4j/tuner/LogisticRegressionTuner.java
@@ -64,7 +64,7 @@ public class LogisticRegressionTuner {
         SimpleMatrix theta = MatrixUtils.weightsToMatrix(bestWeights);
 
         // reduce the learning rate to 10% over the run
-        double lrDelta = (learningRate / maxIterations) * 0.9;
+        double lrDelta = 0; //(learningRate / maxIterations) * 0.9;
 
         for (int it=0; it<maxIterations; it++) {
 
