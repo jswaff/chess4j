0462b63026ae42a571555b2638819013f9fe44e3
James Swafford <j.f.swafford@gmail.com>
2023-12-28 19:57:54 -0500

    record material only for tuner eval

--- chess4j-java/src/main/java/com/jamesswafford/chess4j/tuner/NeuralNetworkTrainer.java
+++ chess4j-java/src/main/java/com/jamesswafford/chess4j/tuner/NeuralNetworkTrainer.java
@@ -19,7 +19,7 @@ public class NeuralNetworkTrainer {
     private static final Logger LOGGER = LogManager.getLogger(NeuralNetworkTrainer.class);
 
     private static final int MINI_BATCH_SIZE = 256;
-    private static final int MAX_DATA_SET_SIZE = 20 * 1000 * 1000;
+    private static final int MAX_DATA_SET_SIZE = 1000 * 1000;
     private static final int MAX_TEST_SET_SIZE = 100000;
 
     public Network train(List<GameRecord> dataSet, double learningRate, int numEpochs) {
