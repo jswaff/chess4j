93f63df87b1ae01918cfcbf7ca73e6ffffcf4285
jswaff <j.f.swafford@gmail.com>
2024-03-11 08:03:32 -0400

    export FEN rather than OHE

--- chess4j-java/src/main/java/com/jamesswafford/chess4j/tuner/BoardToNetwork.java
+++ chess4j-java/src/main/java/com/jamesswafford/chess4j/tuner/BoardToNetwork.java
@@ -5,23 +5,25 @@ import com.jamesswafford.chess4j.pieces.*;
 
 public class BoardToNetwork {
 
-    public static final int NUM_INPUTS = 10;
+    public static final int NUM_INPUTS = 768;
 
     public static double[][] transform(Board board) {
         double[][] data = new double[NUM_INPUTS][1];
 
         for (int i=0;i<64;i++) {
             Piece p = board.getPiece(i);
-            if (Rook.WHITE_ROOK.equals(p)) data[0][0]++;
-            else if (Rook.BLACK_ROOK.equals(p)) data[1][0]++;
-            else if (Knight.WHITE_KNIGHT.equals(p)) data[2][0]++;
-            else if (Knight.BLACK_KNIGHT.equals(p)) data[3][0]++;
-            else if (Bishop.WHITE_BISHOP.equals(p)) data[4][0]++;
-            else if (Bishop.BLACK_BISHOP.equals(p)) data[5][0]++;
-            else if (Queen.WHITE_QUEEN.equals(p)) data[6][0]++;
-            else if (Queen.BLACK_QUEEN.equals(p)) data[7][0]++;
-            else if (Pawn.WHITE_PAWN.equals(p)) data[8][0]++;
-            else if (Pawn.BLACK_PAWN.equals(p)) data[9][0]++;
+            if (Rook.WHITE_ROOK.equals(p)) data[i][0] = 1;
+            else if (Rook.BLACK_ROOK.equals(p)) data[64+i][0] = 1;
+            else if (Knight.WHITE_KNIGHT.equals(p)) data[128+i][0] = 1;
+            else if (Knight.BLACK_KNIGHT.equals(p)) data[192+i][0] = 1;
+            else if (Bishop.WHITE_BISHOP.equals(p)) data[256+i][0] = 1;
+            else if (Bishop.BLACK_BISHOP.equals(p)) data[320+i][0] = 1;
+            else if (Queen.WHITE_QUEEN.equals(p)) data[384+i][0] = 1;
+            else if (Queen.BLACK_QUEEN.equals(p)) data[448+i][0] = 1;
+            else if (King.WHITE_KING.equals(p)) data[512+i][0] = 1;
+            else if (King.BLACK_KING.equals(p)) data[576+i][0] = 1;
+            else if (Pawn.WHITE_PAWN.equals(p)) data[640+i][0] = 1;
+            else if (Pawn.BLACK_PAWN.equals(p)) data[704+i][0] = 1;
         }
 
         return data;
