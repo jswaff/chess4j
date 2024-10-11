2b1a2bab3643761ae3f3bae7d43ba235b876fa96
James Swafford <j.f.swafford@gmail.com>
2024-10-11 10:26:30 -0400

    remove unused exceptions

--- src/main/java/dev/jamesswafford/chess4j/exceptions/GameRecordNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package dev.jamesswafford.chess4j.exceptions;
-
-public class GameRecordNotFoundException extends RuntimeException {
-
-    public GameRecordNotFoundException(String msg) {
-        super(msg);
-    }
-}
