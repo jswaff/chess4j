7d5573d76a6669747a91be7919c944ab5c0628d1
James Swafford <j.f.swafford@gmail.com>
2024-11-05 20:01:29 -0500

    update FEN when re-labeling to end of PV

--- chess4j-java/src/main/java/dev/jamesswafford/chess4j/nn/FENLabeler.java
+++ chess4j-java/src/main/java/dev/jamesswafford/chess4j/nn/FENLabeler.java
@@ -1,8 +1,10 @@
 package dev.jamesswafford.chess4j.nn;
 
 import dev.jamesswafford.chess4j.board.Board;
+import dev.jamesswafford.chess4j.board.Move;
 import dev.jamesswafford.chess4j.eval.Eval;
 import dev.jamesswafford.chess4j.eval.EvalWeights;
+import dev.jamesswafford.chess4j.io.FENBuilder;
 import dev.jamesswafford.chess4j.io.FENRecord;
 import dev.jamesswafford.chess4j.search.AlphaBetaSearch;
 import dev.jamesswafford.chess4j.search.Search;
@@ -37,6 +39,11 @@ public class FENLabeler {
         SearchParameters parameters = new SearchParameters(depth, -CHECKMATE, CHECKMATE);
         search.initialize();
         int score = search.search(board, parameters);
+        List<Move> pv = search.getPv();
+        for (Move pvMove : pv) {
+            board.applyMove(pvMove);
+        }
+        fenRecord.setFen(FENBuilder.createFen(board, false));
         fenRecord.setEval(score);
     }
 }
