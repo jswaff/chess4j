0794b0ded6786f26f7d1ee80b45fc8e8cef1839f
James Swafford <j.f.swafford@gmail.com>
2025-02-15 10:06:58 -0500

    moving methods that modify accmulators to NnueAccumulators

--- chess4j-java/src/main/java/dev/jamesswafford/chess4j/nn/NeuralNetwork.java
+++ chess4j-java/src/main/java/dev/jamesswafford/chess4j/nn/NeuralNetwork.java
@@ -1,7 +1,6 @@
 package dev.jamesswafford.chess4j.nn;
 
 import dev.jamesswafford.chess4j.board.Board;
-import dev.jamesswafford.chess4j.pieces.*;
 
 import java.io.*;
 
@@ -9,10 +8,10 @@ public class NeuralNetwork {
     public static final int NN_SIZE_L1 = 1536;
     public static final int NN_SIZE_L2 = 1;
 
-    private final double[] W0;
-    private final double[] B0;
-    private final double[] W1;
-    private final double[] B1;
+    public final double[] W0;
+    public final double[] B0;
+    public final double[] W1;
+    public final double[] B1;
 
     public NeuralNetwork() {
         W0 = new double[768 * NN_SIZE_L1];
@@ -50,7 +49,7 @@ public class NeuralNetwork {
 
     public int eval(Board board) {
 
-        populateAccumulators(board, board.getNnueAccumulators()); // TODO: incremental updates
+        board.getNnueAccumulators().populate(board, this); // TODO: incremental updates
         assert(verifyAccumulators(board));
 
         // set layer 1 features from accumulators
@@ -84,78 +83,6 @@ public class NeuralNetwork {
         return val;
     }
 
-    private boolean verifyAccumulators(Board board) {
-        //  populate local copy of accumulators
-        NnueAccumulators accumulators = new NnueAccumulators();
-        populateAccumulators(board, accumulators);
-
-        // compare local copy to copy in Board
-        return accumulators.equalsWithinEpsilon(board.getNnueAccumulators());
-    }
-
-    private void populateAccumulators(Board board, NnueAccumulators accumulators) {
-
-        // initialize with bias term
-        for (int o=0;o<NN_SIZE_L1;o++) {
-            accumulators.set(0, o, B0[o]);
-            accumulators.set(1, o, B0[o]);
-        }
-
-        for (int sq=0;sq<64;sq++) {
-            if (board.getPiece(sq) != null) {
-                addPiece(board, sq, accumulators);
-            }
-        }
-    }
-
-    private void addPiece(Board board, int sq, NnueAccumulators accumulators) {
-        Piece piece = board.getPiece(sq);
-
-        int pieceColor, pieceType;
-        if (piece.isWhite()) {
-            pieceColor = 0;
-            if (piece.equals(Rook.WHITE_ROOK)) {
-                pieceType = 0;
-            } else if (piece.equals(Knight.WHITE_KNIGHT)) {
-                pieceType = 1;
-            } else if (piece.equals(Bishop.WHITE_BISHOP)) {
-                pieceType = 2;
-            } else if (piece.equals(Queen.WHITE_QUEEN)) {
-                pieceType = 3;
-            } else if (piece.equals(King.WHITE_KING)) {
-                pieceType = 4;
-            } else {
-                pieceType = 5; // pawn
-            }
-        } else {
-            pieceColor = 1;
-            if (piece.equals(Rook.BLACK_ROOK)) {
-                pieceType = 0;
-            } else if (piece.equals(Knight.BLACK_KNIGHT)) {
-                pieceType = 1;
-            } else if (piece.equals(Bishop.BLACK_BISHOP)) {
-                pieceType = 2;
-            } else if (piece.equals(Queen.BLACK_QUEEN)) {
-                pieceType = 3;
-            } else if (piece.equals(King.BLACK_KING)) {
-                pieceType = 4;
-            } else {
-                pieceType = 5; // pawn
-            }
-        }
-
-        int index_w = pieceType * 2 + pieceColor;
-        int feature_w = (64 * index_w) + sq;
-
-        int index_b = pieceType * 2 + (1 - pieceColor);
-        int feature_b = (64 * index_b) + (sq ^ 56);
-
-        for (int o=0;o<NN_SIZE_L1;o++) {
-            accumulators.add(0, o, W0[NN_SIZE_L1 * feature_w + o]);
-            accumulators.add(1, o, W0[NN_SIZE_L1 * feature_b + o]);
-        }
-    }
-
     private void computeLayer(double[] I, double[] W, double[] B, double[] O, boolean withClamp) {
         for (int o=0;o<O.length;o++) {
             double sum = B[o];
@@ -171,4 +98,14 @@ public class NeuralNetwork {
             }
         }
     }
+
+    private boolean verifyAccumulators(Board board) {
+        //  populate local copy of accumulators
+        NnueAccumulators accumulators = new NnueAccumulators();
+        accumulators.populate(board, this);
+
+        // compare local copy to copy in Board
+        return accumulators.equalsWithinEpsilon(board.getNnueAccumulators());
+    }
+
 }
--- chess4j-java/src/main/java/dev/jamesswafford/chess4j/nn/NnueAccumulators.java
+++ chess4j-java/src/main/java/dev/jamesswafford/chess4j/nn/NnueAccumulators.java
@@ -1,8 +1,19 @@
 package dev.jamesswafford.chess4j.nn;
 
+import dev.jamesswafford.chess4j.board.Board;
+import dev.jamesswafford.chess4j.pieces.*;
+
+import static dev.jamesswafford.chess4j.nn.NeuralNetwork.NN_SIZE_L1;
+import static dev.jamesswafford.chess4j.pieces.Bishop.*;
+import static dev.jamesswafford.chess4j.pieces.King.*;
+import static dev.jamesswafford.chess4j.pieces.Knight.*;
+import static dev.jamesswafford.chess4j.pieces.Pawn.*;
+import static dev.jamesswafford.chess4j.pieces.Queen.*;
+import static dev.jamesswafford.chess4j.pieces.Rook.*;
+
 public class NnueAccumulators {
 
-    private final double[][] accumulators = new double[2][NeuralNetwork.NN_SIZE_L1];
+    private final double[][] accumulators = new double[2][NN_SIZE_L1];
 
     private final static double epsilon = 0.00001;
 
@@ -16,6 +27,69 @@ public class NnueAccumulators {
 
     public void set(int ind1, int ind2, double val) {
         accumulators[ind1][ind2] = val;
+    }
+
+    private void addPiece(Board board, int sq, NeuralNetwork nn) {
+        Piece piece = board.getPiece(sq);
+
+        int pieceColor, pieceType;
+        if (piece.isWhite()) {
+            pieceColor = 0;
+            if (piece.equals(WHITE_ROOK)) {
+                pieceType = 0;
+            } else if (piece.equals(WHITE_KNIGHT)) {
+                pieceType = 1;
+            } else if (piece.equals(WHITE_BISHOP)) {
+                pieceType = 2;
+            } else if (piece.equals(WHITE_QUEEN)) {
+                pieceType = 3;
+            } else if (piece.equals(WHITE_KING)) {
+                pieceType = 4;
+            } else {
+                pieceType = 5; // pawn
+            }
+        } else {
+            pieceColor = 1;
+            if (piece.equals(BLACK_ROOK)) {
+                pieceType = 0;
+            } else if (piece.equals(BLACK_KNIGHT)) {
+                pieceType = 1;
+            } else if (piece.equals(BLACK_BISHOP)) {
+                pieceType = 2;
+            } else if (piece.equals(BLACK_QUEEN)) {
+                pieceType = 3;
+            } else if (piece.equals(BLACK_KING)) {
+                pieceType = 4;
+            } else {
+                pieceType = 5; // pawn
+            }
+        }
+
+        int index_w = pieceType * 2 + pieceColor;
+        int feature_w = (64 * index_w) + sq;
+
+        int index_b = pieceType * 2 + (1 - pieceColor);
+        int feature_b = (64 * index_b) + (sq ^ 56);
+
+        for (int o=0;o<NN_SIZE_L1;o++) {
+            add(0, o, nn.W0[NN_SIZE_L1 * feature_w + o]);
+            add(1, o, nn.W0[NN_SIZE_L1 * feature_b + o]);
+        }
+    }
+
+    public void populate(Board board, NeuralNetwork nn) {
+
+        // initialize with bias term
+        for (int o=0;o<NN_SIZE_L1;o++) {
+            set(0, o, nn.B0[o]);
+            set(1, o, nn.B0[o]);
+        }
+
+        for (int sq=0;sq<64;sq++) {
+            if (board.getPiece(sq) != null) {
+                addPiece(board, sq, nn);
+            }
+        }
     }
 
     public boolean equalsWithinEpsilon(NnueAccumulators that) {
