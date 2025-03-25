2a94d09928e5c5f23346334f8b9cf70c27311f45
James Swafford <j.f.swafford@gmail.com>
2025-03-25 13:23:48 -0400

    re-enable pawn hash comparison

--- chess4j-java/src/main/java/dev/jamesswafford/chess4j/search/AlphaBetaSearch.java
+++ chess4j-java/src/main/java/dev/jamesswafford/chess4j/search/AlphaBetaSearch.java
@@ -214,14 +214,13 @@ public class AlphaBetaSearch implements Search {
             // compare the pawn hash table stats
             long javaPawnProbes = TTHolder.getInstance().getPawnHashTable().getNumProbes();
             long javaPawnHits = TTHolder.getInstance().getPawnHashTable().getNumHits();
-            // FIXME - not working when NN is enabled?
-            /*if (javaPawnProbes != nativePawnProbes || javaPawnHits != nativePawnHits) {
+            if (javaPawnProbes != nativePawnProbes || javaPawnHits != nativePawnHits) {
                 LOGGER.error("pawn hash stats not equal! "
                         + "java pawn probes: " + javaPawnProbes + ", native pawn probes: " + nativePawnProbes
                         + ", java pawn hits: " + javaPawnHits + ", native pawn hits: " + nativePawnHits
                         + ", params: " + searchParameters);
                 return false;
-            }*/
+            }
 
             // compare node counts
             if (searchStats.nodes != nativeStats.nodes || searchStats.qnodes != nativeStats.qnodes) {
