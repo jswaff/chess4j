cecf6a702049f9d6e47ade72a1bde9574e7439ef
James Swafford <j.f.swafford@gmail.com>
2025-09-26 09:41:02 -0400

    working

--- src/main/java/dev/jamesswafford/chess4j/nn/NeuralNetwork.java
+++ src/main/java/dev/jamesswafford/chess4j/nn/NeuralNetwork.java
@@ -101,41 +101,36 @@ public class NeuralNetwork {
         }
 
         ////////////////////////////////////////
-/*
-    const __m256i one = _mm256_set1_epi16(1);
-    for (int i=0;i<NN_SIZE_L2;i++) {
-        __m256i sum0 = _mm256_setzero_si256();
-        for (int j=0;j<NN_SIZE_L1*2;j+=32) {
-            const __m256i inp = _mm256_loadu_si256((__m256i*) &L1[j]);
-            const __m256i wei = _mm256_loadu_si256((__m256i*) &nn->W1[i * (NN_SIZE_L1*2) + j]);
-            const __m256i dot = _mm256_madd_epi16(_mm256_maddubs_epi16(inp, wei), one);
-            sum0 = _mm256_add_epi32(sum0, dot);
-        }
-
-        const __m128i sum128lo = _mm256_castsi256_si128(sum0);
-        const __m128i sum128hi = _mm256_extracti128_si256(sum0, 1);
-        __m128i sum128 = _mm_add_epi32(sum128lo, sum128hi);
-        sum128 = _mm_add_epi32(sum128, _mm_shuffle_epi32(sum128, _MM_PERM_ABCD));
-        sum128 = _mm_add_epi32(sum128, _mm_shuffle_epi32(sum128, _MM_PERM_CDAB));
-        L2[i] = _mm_cvtsi128_si32(sum128) + nn->B1[i];
-    }
- */
         int[] L2_1 = new int[NN_SIZE_L2];
 
-        VectorSpecies<Short> species256 = ShortVector.SPECIES_256;
-//        ShortVector one = ShortVector.broadcast(species256, 1);
-//        assert(one.length()==16);
+        VectorSpecies<Byte> BYTE_SPEC = ByteVector.SPECIES_256;
+        VectorSpecies<Short> SHORT_SPEC = ShortVector.SPECIES_256;
+        VectorSpecies<Integer> INT_SPEC = IntVector.SPECIES_256;
 
         for (int i=0;i<NN_SIZE_L2;i++) {
-            ShortVector vector256 = ShortVector.zero(species256);
-            for (int j=0;j<(NN_SIZE_L1*2);j++) {
-                // load 256 bits (32 bytes) starting at L1[j]
-                //ShortVector inp = ShortVector.fromArray(species256, L1, j);
+            IntVector sum32 = IntVector.zero(INT_SPEC);
+            for (int j=0;j<(NN_SIZE_L1*2);j+=BYTE_SPEC.length()) {
+                // sum += W1[i * (NN_SIZE_L1*2) + j] * L1[j];
+                ByteVector inp = ByteVector.fromArray(BYTE_SPEC, L1, j);
+                ByteVector wei = ByteVector.fromArray(BYTE_SPEC, W1, i * (NN_SIZE_L1 * 2) + j);
+
+                // multiply into 16 bit vector
+                ShortVector inpLo = inp.convert(VectorOperators.B2S, 0).reinterpretAsShorts();
+                ShortVector inpHi = inp.convert(VectorOperators.B2S, 1).reinterpretAsShorts();
+                ShortVector weiLo = wei.convert(VectorOperators.B2S, 0).reinterpretAsShorts();
+                ShortVector weiHi = wei.convert(VectorOperators.B2S, 1).reinterpretAsShorts();
+                ShortVector dotLo = inpLo.mul(weiLo);
+                ShortVector dotHi = inpHi.mul(weiHi);
+
+                // add the lanes up
+                sum32 = sum32.add(dotLo.convert(VectorOperators.S2I, 0));
+                sum32 = sum32.add(dotLo.convert(VectorOperators.S2I, 1));
+                sum32 = sum32.add(dotHi.convert(VectorOperators.S2I, 0));
+                sum32 = sum32.add(dotHi.convert(VectorOperators.S2I, 1));
             }
 
-            L2_1[i] = vector256.reduceLanes(VectorOperators.ADD) + B1[i];
+            L2_1[i] = sum32.reduceLanes(VectorOperators.ADD) + B1[i];
         }
-
 
         assert(Arrays.equals(L2, L2_1));
         ////////////////////////////////////////
